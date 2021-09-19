e0f490c735659909abb2c9ceb8ad3d0af950cbf3
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-19 17:48:28 -0500

    implement updating and deleting items in cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,7 +4,7 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
-import { useStoreContext} from '../utils/GlobalState';
+import { useStoreContext } from '../utils/GlobalState';
 import Cart from '../components/Cart';
 import {
   REMOVE_FROM_CART,
@@ -16,13 +16,13 @@ import {
 function Detail() {
   const [state, dispatch] = useStoreContext();
 
-  const {id} = useParams();
+  const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
-  const {loading, data} = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -36,11 +36,29 @@ function Detail() {
   }, [products, data, dispatch, id])
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: {...currentProduct, purchaseQuantity: 1}
-    })
-  }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -54,7 +72,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+              </button>
           </p>
 
           <img
