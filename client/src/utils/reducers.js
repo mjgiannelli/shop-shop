99a36114383dd082dadd2fae4f199a61864c5bb0
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-18 21:51:04 -0500

    create categories test and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -5,15 +5,20 @@ import {
 } from './actions';
 
 export const reducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         //if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
         case UPDATE_PRODUCTS:
             return {
                 ...state,
                 products: [...action.products],
             };
-            //if it's none of these actions, do not update state at all and keep things the same!
-            default: 
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories],
+            }
+        //if it's none of these actions, do not update state at all and keep things the same!
+        default:
             return state;
     }
 }
