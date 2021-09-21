631eca30746d7d9ebecc50e71635e54e1fab4c7f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-20 20:52:17 -0500

    add redirect to stripe checkout on the front end

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,14 +5,21 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
 
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
     useEffect(() => {
         async function getCart() {
             const cart = await idbPromise('cart', 'get');
-            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart]});
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
         };
 
         if (!state.cart.length) {
@@ -23,6 +30,14 @@ const Cart = () => {
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
     }
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            })
+        }
+    }, [data]);
 
     function calculateTotal() {
         let sum = 0;
@@ -41,6 +56,20 @@ const Cart = () => {
         );
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
+
     return (
         <div className="cart">
             <div className="close" onClick={toggleCart}>[close]</div>
@@ -54,7 +83,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
