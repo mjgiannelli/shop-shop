fa36f205d6596f9d5612a33d4632d3f68af2e1a7
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-20 21:18:00 -0500

    create success component to save order in the database

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -5,6 +5,30 @@ import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from "../utils/helpers";
 
 function Success() {
+    const [addOrder] = useMutation(ADD_ORDER);
+
+    useEffect(() => {
+        async function saveOrder() {
+            const cart = await idbPromise('cart', 'get');
+            const products = cart.map(item => item._id);
+
+            if (products.length) {
+                const { data } = await addOrder({ variables: { products }});
+                const productData = data.addOrder.products;
+
+                productData.forEach((item) => {
+                    idbPromise('cart', 'delete', item);
+                });
+            }
+
+            setTimeout(() => {
+                window.location.assign('/');
+            }, 3000);
+        }
+
+        saveOrder();
+    }, [addOrder]);
+
     return (
       <div>
         <Jumbotron>
