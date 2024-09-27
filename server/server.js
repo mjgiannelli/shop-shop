5a70e5ffa02e8ef377aa311a42d1e37e3781f468
Mark Giannelli <giannellimj@gmail.com>
2024-09-27 16:04:30 -0500

    fix port for graphql

--- server/server.js
+++ server/server.js
@@ -33,6 +33,10 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    if (process.env.NODE_ENV === 'production') {
+      console.log(`Use GraphQL at https://${process.env.RENDER_EXTERNAL_URL}${graphqlPath}`);
+    } else {
+      console.log(`Use GraphQL at http://localhost:${PORT}${graphqlPath}`);
+    }
   });
 });
